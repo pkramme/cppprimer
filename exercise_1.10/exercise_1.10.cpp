b8ee4e17e4aad3ea21bdde8d51b43118d940450d
Paul Kramme <pjkramme@gmail.com>
2016-05-28 21:46:38 +0200

    Add exercise_1.10

---
+++ exercise_1.10/exercise_1.10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+     int value1 = 10;
+     while(value1 >= 0)
+     {
+          std::cout << value1 << std::endl;
+          --value1;
+     }
+     return 0;
+}
