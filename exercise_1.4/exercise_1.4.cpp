c8a24d68b6fab5941172dbfdf22ad0d2c68346b6
Paul Kramme <pjkramme@gmail.com>
2016-05-28 21:17:08 +0200

    Fix * instead of +

--- exercise_1.4/exercise_1.4.cpp
+++ exercise_1.4/exercise_1.4.cpp
@@ -4,6 +4,6 @@ int main()
 {
      int value1 = 0, value2 = 0;
      std::cin >> value1 >> value2;
-     std::cout << "The product of " << value1 << " and " << value2 << " equals " << value1 + value2 << std::endl;
+     std::cout << "The product of " << value1 << " and " << value2 << " equals " << value1 * value2 << std::endl;
      return 0;
 }
